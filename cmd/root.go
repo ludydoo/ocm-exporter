11451b809542ad0f0fed947bc1d2dddbee7fe593
Ludovic Cleroux <ludydoo@gmail.com>
2023-05-22 12:06:35 +0200

    cleanup

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	sdk "github.com/openshift-online/ocm-sdk-go"
+	"github.com/openshift-online/ocm-sdk-go/logging"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
@@ -19,6 +20,8 @@ const (
 	flatOrganizationIDShort = "o"
 	flagOCMTokenPath        = "ocm-token-path"
 	flagOCMTokenPathShort   = "t"
+	flagDebugLog            = "debug"
+	flagDebugLogShort       = "d"
 	envOCMToken             = "OCM_TOKEN"
 )
 
@@ -26,46 +29,39 @@ var rootCmd = &cobra.Command{
 	Use:   "ocm-exporter",
 	Short: "Starts the OCM metrics exporter",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		ocmToken, err := getOCMToken(cmd)
-		if err != nil {
+
+		var (
+			ocmToken   string
+			port       string
+			orgID      string
+			debug      bool
+			logger     *logging.GoLogger
+			connection *sdk.Connection
+			err        error
+		)
+
+		if ocmToken, err = getOCMTokenFlag(cmd); err != nil {
 			return err
 		}
-
-		port, err := getPort(cmd)
-		if err != nil {
+		if port, err = getPortFlag(cmd); err != nil {
 			return err
 		}
-
-		orgID, err := getOrgID(cmd)
-		if err != nil {
+		if orgID, err = getOrgIDFlag(cmd); err != nil {
 			return err
 		}
-
-		logger, err := sdk.NewGoLoggerBuilder().
-			Debug(true).
-			Build()
-
-		if err != nil {
+		if debug, err = getDebugFlag(cmd); err != nil {
+			return err
+		}
+		if logger, err = sdk.NewGoLoggerBuilder().Debug(debug).Build(); err != nil {
 			return fmt.Errorf("failed to create logger: %v", err)
 		}
-
-		connection, err := sdk.NewConnectionBuilder().
-			Logger(logger).
-			Tokens(ocmToken).
-			Build()
-		if err != nil {
+		if connection, err = sdk.NewConnectionBuilder().Logger(logger).Tokens(ocmToken).Build(); err != nil {
 			return fmt.Errorf("failed to create ocm connection: %v", err)
 		}
 		defer connection.Close()
 
-		if orgID == "" {
-			// Get organization of current user:
-			userConn, err := connection.AccountsMgmt().V1().CurrentAccount().Get().Send()
-			if err != nil {
-				return fmt.Errorf("failed to retrieve current user information: %v", err)
-			}
-			userOrg, _ := userConn.Body().GetOrganization()
-			orgID = userOrg.ID()
+		if orgID, err = getOrgID(orgID, connection); err != nil {
+			return err
 		}
 
 		orgCollection := connection.AccountsMgmt().V1().Organizations().Organization(orgID)
@@ -77,10 +73,22 @@ var rootCmd = &cobra.Command{
 
 		collector := newOcmCollector(quotaClient)
 		prometheus.MustRegister(collector)
-
 		http.Handle("/metrics", promhttp.Handler())
 		return http.ListenAndServe(":"+port, nil)
 	},
+}
+
+func getOrgID(orgID string, connection *sdk.Connection) (string, error) {
+	if orgID == "" {
+		// Get organization of current user:
+		userConn, err := connection.AccountsMgmt().V1().CurrentAccount().Get().Send()
+		if err != nil {
+			return "", fmt.Errorf("failed to retrieve current user information: %v", err)
+		}
+		userOrg, _ := userConn.Body().GetOrganization()
+		orgID = userOrg.ID()
+	}
+	return orgID, nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -99,9 +107,11 @@ func init() {
 	rootCmd.Flags().StringP(flagOrganizationID, flatOrganizationIDShort, "", "Organization ID to query quotas for")
 
 	rootCmd.Flags().StringP(flagOCMTokenPath, flagOCMTokenPathShort, "", "Path to file containing OCM token")
+
+	rootCmd.Flags().BoolP(flagDebugLog, flagDebugLogShort, false, "Enable debug logging")
 }
 
-func getOCMToken(cmd *cobra.Command) (string, error) {
+func getOCMTokenFlag(cmd *cobra.Command) (string, error) {
 	ocmToken := os.Getenv(envOCMToken)
 	ocmTokenPath, err := cmd.Flags().GetString(flagOCMTokenPath)
 	if err != nil {
@@ -117,7 +127,7 @@ func getOCMToken(cmd *cobra.Command) (string, error) {
 	return ocmToken, nil
 }
 
-func getPort(cmd *cobra.Command) (string, error) {
+func getPortFlag(cmd *cobra.Command) (string, error) {
 	port, err := cmd.Flags().GetString(flagPort)
 	if err != nil {
 		return "", fmt.Errorf("failed to get %s flag: %v", flagPort, err)
@@ -128,10 +138,18 @@ func getPort(cmd *cobra.Command) (string, error) {
 	return port, nil
 }
 
-func getOrgID(cmd *cobra.Command) (string, error) {
+func getOrgIDFlag(cmd *cobra.Command) (string, error) {
 	orgID, err := cmd.Flags().GetString(flagOrganizationID)
 	if err != nil {
 		return "", fmt.Errorf("failed to get %s flag: %v", flagOrganizationID, err)
 	}
 	return orgID, nil
+}
+
+func getDebugFlag(cmd *cobra.Command) (bool, error) {
+	debug, err := cmd.Flags().GetBool(flagDebugLog)
+	if err != nil {
+		return false, fmt.Errorf("failed to get %s flag: %v", flagDebugLog, err)
+	}
+	return debug, nil
 }
